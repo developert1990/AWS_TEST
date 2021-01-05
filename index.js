a1eb5ec3307737ca4f2fda2e888af5fabde63b3a
minotaurrr <kwangmin.kim@hotmail.com>
2021-01-05 22:07:15 +1100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const server = app.listen(7777);
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.send('API working fine');
+});
+
+app.get('/second-route', (req, res) => {
+    res.send('second route working fine');
 });
 
 module.exports = {
