2e40ee01512f456f10fa91954097b727aa4f7691
developert1990 <developert.hong@gmail.com>
2021-01-05 21:19:49 -0700

    changed route

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 const server = app.listen(7777);
 
-app.get('/', (req, res) => {
-    res.send('API working fine');
-});
-
 app.get('/second-route', (req, res) => {
     res.send('second route fine');
 });
