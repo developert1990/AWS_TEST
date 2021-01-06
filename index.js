e452857679f984a65ce0cd5a52f42dfe1b0751dd
developert1990 <developert.hong@gmail.com>
2021-01-05 21:21:20 -0700

    fixed route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const server = app.listen(7777);
 
+app.get('/', (req, res) => {
+    res.send('API working fine');
+});
+
 app.get('/second-route', (req, res) => {
     res.send('second route fine');
 });
